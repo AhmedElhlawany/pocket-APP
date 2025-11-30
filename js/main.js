15fb28bca95e470dca1b75f9cc8717ab1d169469
AhmedElhlawany <ahmedelhalawany429@gmail.com>
2025-12-01 00:58:22 +0200

    design update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const input = document.getElementById("whatsapp");
 const formWrapper = document.getElementById("form-wrapper");
 const successMessage = document.getElementById("success-message");
+const inputContainer = document.querySelector(".input-container");
 
 input.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
@@ -12,6 +13,15 @@ input.addEventListener("keydown", function (e) {
       formWrapper.style.display  = "none";
       successMessage.classList.remove("hidden");
     }
+  }
+});
+input.addEventListener("input", () => {
+  const regex = /^(010|011|012|015)\d{8}$/;
+
+  if (regex.test(input.value)) {
+    inputContainer.style.border = "2px solid green";
+  } else {
+    inputContainer.style.border = "2px solid red";
   }
 });
 
@@ -27,5 +37,16 @@ input2.addEventListener("keydown", function (e) {
       formWrapper2.style.display  = "none";
       successMessage2.classList.remove("hidden");
     }
+  }
+});
+
+const inputContainer2 = document.querySelector(".input-container2");
+input2.addEventListener("input", () => {
+  const regex = /^(010|011|012|015)\d{8}$/;
+
+  if (regex.test(input2.value)) {
+    inputContainer2.style.border = "2px solid green";
+  } else {
+    inputContainer2.style.border = "2px solid red";
   }
 });
